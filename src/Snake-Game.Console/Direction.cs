387e59a7e930dd40df9badd7df5bccd6b9e626d3
Elsa Girardo <egirardo@hotmail.com>
2026-04-15 21:32:56 +0200

    Snake-Game Program

---
+++ src/Snake-Game.Console/Direction.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Game.Console;
+
+internal enum Direction
+{
+    Up,
+    Down,
+    Left,
+    Right
+}
